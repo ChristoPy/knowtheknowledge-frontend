fd64a2a39038e0e6f067dcf686f9dc360069e572
Christopher <christo_campiglia@hotmail.com>
2018-02-16 20:47:39 -0200

    [Fix] No debug info

--- src/Auth.js
+++ src/Auth.js
@@ -3,11 +3,11 @@ export default {
 	CheckToken (To, From, Next, Fallback) {
 
 		if (window.sessionStorage.u && window.sessionStorage.t) {
-			console.log ("kkkk")
+
 			Next ("/");
 		}
 		else {
-			console.log ("oooooo")
+
 			Next ();
 		}
 	}
