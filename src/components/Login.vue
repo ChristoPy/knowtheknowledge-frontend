b3c5704c0e267816d865f68e48c6c78385f3aa0f
Christopher <christo_campiglia@hotmail.com>
2018-02-16 17:30:42 -0200

    [Add] Login feature

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,54 +1,70 @@
 <template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 ml-auto mr-auto">
+				<div class="card">
+					<h2 class="card-title text-center">Login</h2> 
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 ml-auto mr-auto">
-			<div class="card">
-				<h2 class="card-title text-center">Login</h2> 
-
-				<form class="form card-body" method="" action="">
-					<div class="form-group bmd-form-group">
-						<div class="input-group">
-							<div class="input-group-prepend mt-2">
-								<span class="input-group-addon">
-									<i class="fa fa-envelope"></i>
-								</span>
+					<form class="form card-body" method="" action="">
+						<div class="form-group bmd-form-group">
+							<div class="input-group">
+								<div class="input-group-prepend mt-2">
+									<span class="input-group-addon">
+										<i class="fa fa-envelope"></i>
+									</span>
+								</div>
+								
+								<input type="text" class="form-control" placeholder="Email..." v-model="Email">
 							</div>
-							
-							<input type="text" class="form-control" placeholder="Email...">
 						</div>
-					</div>
 
-					<div class="form-group bmd-form-group">
-						<div class="input-group">
-							<div class="input-group-prepend mt-2">
-								<span class="input-group-addon">
-									<i class="fa fa-lock"></i>
-								</span>
+						<div class="form-group bmd-form-group">
+							<div class="input-group">
+								<div class="input-group-prepend mt-2">
+									<span class="input-group-addon">
+										<i class="fa fa-lock"></i>
+									</span>
+								</div>
+								
+								<input type="password" placeholder="Password..." class="form-control" v-model="Password">
 							</div>
-							
-							<input type="password" placeholder="Password..." class="form-control">
 						</div>
-					</div>
 
-					<div class="text-center">
-						<a href="#" class="btn btn-primary btn-round">Login</a>
-					</div>
-				</form>
+						<div class="text-center">
+							<a href="#" class="btn btn-primary btn-round" @click="TryLogin (Email, Password)">Login</a>
+						</div>
+					</form>
 
-				<div class="card-footer mr-auto ml-auto mt-2">
-					<a href="/signup">Create Account</a>
+					<div class="card-footer mr-auto ml-auto mt-2">
+						<a href="/signup">Create Account</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-
 </template>
 
 <script>
 	export default {
-		name: "Login"
+		name: "Login",
+		data () {
+
+			return {
+				Email: "",
+				Password: ""
+			}
+		},
+		methods: {
+
+			TryLogin: function (Email, Password) {
+
+				$.post ("https://knowapi-knowapi.wedeploy.io/knowapi/v1/login", 
+					{email: Email, password: Password}, function (Response) {
+
+					console.log (Response);
+				});
+			}
+		}
 	}
 </script>
 
