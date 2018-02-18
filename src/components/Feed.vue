e4ede476318f9572c34eaf03608f33afa88bc327
Christopher <christo_campiglia@hotmail.com>
2018-02-18 07:29:51 -0300

    [UPDATE] Font Awesome from 4 to 5

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -3,17 +3,29 @@
 		<div class="row">
 			<div class="col-md-10 ml-auto mr-auto">
 
-					<div v-for="Post of Posts" class="card">
+				<div v-for="Post of Posts" class="card">
+					<div class="card-body">
+						<h3 class="card-title text-center title">
+							{{Post.title}}
+						</h3>
 
-						<ion-item>
-							{{Post}}
-						</ion-item>
+						<h5 class="text-center">
+							By: <a href="#">{{Post.user}}</a>
+						</h5>
 
+						<p class="card-description text-center">
+							{{Post.postMessage}}
+						</p>
+
+
+						<div class="card-text">
+							<i class="fas fa-calendar-alt"></i>
+							<small class="text-muted ml-1">{{Post.createdAt}}</small>
+						</div>
 					</div>
 
-<!-- 				<div class="card">
-					<h2 class="card-title text-center">Welcome</h2> 
-				</div> -->
+				</div>
+
 			</div>
 		</div>
 	</div>
@@ -44,7 +56,20 @@
 					{})
 					.always (function (Response) {
 
-						console.log (Response);
+						Response.posts = [
+							{
+								title: "Lorem ipsum",
+								user: "Lorem",
+								postMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+								createdAt: new Date ()
+							},
+							{
+								title: "Sed ut perspiciatis",
+								user: "ipsum",
+								postMessage: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+								createdAt: new Date ()
+							}
+							]
 
 						const JSONResponse = Response.responseJSON;
 
@@ -54,7 +79,7 @@
 						}
 						else {
 
-							this.Posts = Response.posts;
+							ME.Posts = Response.posts;
 						}
 				});
 			},
