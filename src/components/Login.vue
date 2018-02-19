eff3005b76a1ccd3927e26b6929a4692624bd65b
Christopher <christo_campiglia@hotmail.com>
2018-02-18 22:56:19 -0300

    [ADD] Navbar add

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,51 +1,63 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-10 ml-auto mr-auto">
-				<div class="card">
-					<h2 class="card-title text-center">Login</h2> 
-
-					<form class="form card-body" method="" action="">
-						<div class="form-group bmd-form-group">
-							<div class="input-group">
-								<div class="input-group-prepend mt-2">
-									<span class="input-group-addon">
-										<i class="fa fa-envelope"></i>
-									</span>
-								</div>
-								
-								<input type="text" class="form-control" placeholder="Email..." v-model="Email">
-							</div>
-						</div>
-
-						<div class="form-group bmd-form-group">
-							<div class="input-group">
-								<div class="input-group-prepend mt-2">
-									<span class="input-group-addon">
-										<i class="fa fa-lock"></i>
-									</span>
-								</div>
-								
-								<input type="password" placeholder="Password..." class="form-control" v-model="Password">
-							</div>
-						</div>
-
-						<div class="text-center">
-							<a href="#" class="btn btn-primary btn-round" @click="TryLogin (Email, Password)">Login</a>
-						</div>
-					</form>
-
-					<div class="card-footer mr-auto ml-auto mt-2">
-						<a href="/signup">Create Account</a>
-					</div>
+	<main>
+		<nav class="navbar fixed-top navbar-expand-lg bg-success">
+			<div class="container">
+				<div class="navbar-translate">
+					<a class="navbar-brand" href="#">
+						<img src="../assets/K.png" class="Logo image-fluid">
+					</a>
 				</div>
+			</div>
+		</nav>
 
-				<div class="alert alert-danger text-center" role="alert" v-if="ErrorName">
-					{{ErrorName}}
+		<div class="container" id="MainContent">
+			<div class="row">
+				<div class="col-md-10 ml-auto mr-auto">
+					<div class="card">
+						<h2 class="card-title text-center">Login</h2> 
+
+						<form class="form card-body" method="" action="">
+							<div class="form-group bmd-form-group">
+								<div class="input-group">
+									<div class="input-group-prepend mt-2">
+										<span class="input-group-addon">
+											<i class="fa fa-envelope"></i>
+										</span>
+									</div>
+									
+									<input type="text" class="form-control" placeholder="Email..." v-model="Email">
+								</div>
+							</div>
+
+							<div class="form-group bmd-form-group">
+								<div class="input-group">
+									<div class="input-group-prepend mt-2">
+										<span class="input-group-addon">
+											<i class="fa fa-lock"></i>
+										</span>
+									</div>
+									
+									<input type="password" placeholder="Password..." class="form-control" v-model="Password">
+								</div>
+							</div>
+
+							<div class="text-center">
+								<a href="#" class="btn btn-primary btn-round" @click="TryLogin (Email, Password)">Login</a>
+							</div>
+						</form>
+
+						<div class="card-footer mr-auto ml-auto mt-2">
+							<a href="/signup">Create Account</a>
+						</div>
+					</div>
+
+					<div class="alert alert-danger text-center" role="alert" v-if="ErrorName">
+						{{ErrorName}}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</main>
 </template>
 
 <script>
@@ -111,4 +123,19 @@
 	}
 </script>
 
-<style></style>
+<style scoped>
+
+.Logo {
+
+	width: 50px;
+	height: 50px;
+	margin-top: -10px;
+}
+
+#MainContent {
+
+	position: relative;
+	margin-top: 70px;
+}
+
+</style>
